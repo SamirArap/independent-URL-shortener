200352683714dddfc024774a0fb778b01907fe16
SamirArap <samir.arap@gmail.com>
2016-10-27 10:14:38 +0200

    deleted base_url

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,7 +3,6 @@ class Url < ApplicationRecord
   validates_format_of :original_url, with: URI::regexp(%w(http https))
   after_create :generate_shortId
 
-  ENV['BASE_URL']='http://localhost:3000/'
 
   def display_url
     ENV['BASE_URL'] + self.short_id
